1a3d21f6c9883ff539c74a05aa005e23f3bd375c
Thomas <tlee066@ucr.edu>
2019-04-29 12:09:58 -0700

    tried some login stuff

---
+++ src/signup.js
@@ -0,0 +1,5 @@
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    //handle errors here
+    var errorCode = error.code;
+    var errorMessage = error.message;
+});
