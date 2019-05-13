587a4b483c18770f753d02a3ca063b60fc22da7c
Damian Turner <damianturner@Buddys-Macbook.fios-router.home>
2019-05-12 17:12:27 -0700

    Fix database reading functions to only read messages once

--- src/public/main.js
+++ src/public/main.js
@@ -16,36 +16,31 @@ function loadRequests(user) {
   // Much more advanced queries are possible if needed
   var query = firebase.firestore()
                 .collection('requests')
-                .where("user", "==", user);
-
-  query.onSnapshot(function(snapshot) {
-    snapshot.docChanges().forEach(function(change) {
-        var message = change.doc.data();
-        // do stuff with message
-	console.log("user: ", message.user);
-        console.log("read comment: ", message.message);
-    });
-  });
+                .where("user", "==", user)
+		.get()
+		.then(function(querySnapshot) {
+			querySnapshot.forEach(function(doc) {
+        			// do stuff with message
+				console.log("user: ", doc.get("user"));
+        			console.log("read comment: ", doc.get("message"));
+    		});
+	});
 }
 
 function getRequestsTotal(user) {
-  // Returns results for a query where user field in the table is equal to user variable passed in
-  // Much more advanced queries are possible if needed
-  var query = firebase.firestore()
+var total = 0;
+
+var query = firebase.firestore()
                 .collection('requests')
-                .where("user", "==", user);
-
-  var total = 0
-
-  query.onSnapshot(function(snapshot) {
-    snapshot.docChanges().forEach(function(change) {
-        var message = change.doc.data();
-        total += parseInt(message.amount);
-        // do stuff with message
-	console.log("user: ", message.user);
-        console.log("current total: ", total);
-    });
-  });
+                .where("user", "==", user)
+                .get()
+                .then(function(querySnapshot) {
+                        querySnapshot.forEach(function(doc) {
+                                total += parseInt(doc.get("amount"));
+                                console.log("curr total: ", total);
+                });
+        });	
+return total;
 }
 
 function saveUser(fullname, useremail) {
