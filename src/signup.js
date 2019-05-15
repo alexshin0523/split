785ceb14b63e4e2ee457ca497799ad3be646714f
Damian Turner <dturn003@ucr.edu>
2019-05-14 19:14:38 -0700

    Cleanup some old files in src directory.

--- src/signup.js
+++
@@ -1,7 +0,0 @@
-var firebase = require("firebase");
-
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    //handle errors here
-    var errorCode = error.code;
-    var errorMessage = error.message;
-});
